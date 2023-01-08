ef8bb1ae0854dc442005a00d50a14ab49e76b229
iamheewonkim <heewonkim.work@gmail.com>
2023-01-08 17:32:05 +0900

    :fire: remove all files to restart the blog

--- @types/image.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.png'
-declare module '*.gif'
-declare module '*.jpeg'
-declare module '*.svg'
-declare module '*.jpg'
--- src/components/hr/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { HrCustomContainer } from './index.style';
-
-function Hr() {
-  return <HrCustomContainer className="custom-hr" />;
-}
-
-export default Hr;
